eab89f522e935d03b712a5a15937c02ca5b0174e
Nicki Ogg <oggnicki@yahoo.com>
2014-07-06 11:55:20 -0400

    Table View

--- HistogramViewer/src/view/TableView.scala
+++ HistogramViewer/src/view/TableView.scala
@@ -1,5 +1,29 @@
 package view
 
-class TableView {
+import javax.swing.JPanel
+import java.awt.GridLayout
 
+class TableView(data: List[Array[String]]) extends JPanel {
+  val dataWidth = data.maxBy(line => line.length).length
+  val dataHeight = data.length
+  
+  /*
+   * Start GUI Creation
+   */
+  val cells = Array.ofDim[TableCell](dataWidth, dataHeight)
+  
+  setLayout(new GridLayout(dataHeight, dataWidth))
+  
+  for (y <- 0 until dataHeight) {
+    val line = data(y)
+    for (x <- 0 until dataWidth) {
+      val cellData = line(x)
+      val tableCell = new TableCell(cellData)
+      cells(x)(y) = tableCell
+      add(tableCell)
+    }
+  }
+  /*
+   * End GUI Creation
+   */
 }
